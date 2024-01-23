ebeb960014b81d277f006592352a4c7e15455a3a
unknown <vaishnavi.salunkhe@kalvium.community>
2024-01-23 11:45:20 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import './App.css';
-import LikeImage from './components/LikeImage';
-import LikePost from './components/LikePost';
+// import LikeImage from './components/LikeImage';
+// import LikePost from './components/LikePost';
+import LikePostHoc from './components/HOCs/LikePostHoc';
+import LikeImgHoc from './components/HOCs/LikeImgHoc';
+import LikeImageRender from './components/Renderprops/LikeImageRender';
+import LikePostRender from './components/Renderprops/LikePostRender';
 
 function App() {
   return (
     <div>
-      <h3>Some Blog</h3>
-      <div className='buttons'>
-        <LikePost/>
-        <LikeImage/>
-      </div>
+      <h1>Blogs post using HOC</h1>
+      <LikeImgHoc/>
+      <LikePostHoc/>
+
+      {/* <h1>Using code duplicity</h1>
+      <LikeImage/>
+      <LikePost/> */}
+
+      <h1>Blogs Post using Render Props</h1>
+      <LikeImageRender/>
+      <LikePostRender/>
+      
     </div>
   );
 }
